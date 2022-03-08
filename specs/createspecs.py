8a865b68e4ff60c9824822c27f6e822c3636cc95
rdlrt <3169068+rdlrt@users.noreply.github.com>
2022-03-08 00:52:57 +0000

    - Updates as per @cardano-community/guild-operators#1355 - Move over specs, grafana-dashboards, topology files from guild-ops repo ( @cardano-community/guild-operators#1369 ) - Add Readme and license

--- specs/createspecs.py
+++ specs/createspecs.py
@@ -2,13 +2,21 @@
 
 import json, pathlib, traceback
 
-inputspecsfile="koiosapi.yaml"
-examplesfile="spec_examples.json"
+inputspecsfile="templates/api-main.yaml"
+apiinfofile="templates/1-api-info.yaml"
+apiparamsfile="templates/2-api-params.yaml"
+apirequestBodiesfile="templates/3-api-requestBodies.yaml"
+apischemafile="templates/4-api-schemas.yaml"
+examplesfile="templates/example-map.json"
 examples=json.loads(pathlib.Path(examplesfile).read_text())
 
 def populate_spec(network, outf):
   """Populate template spec using network initial and write to outf file"""
   template = pathlib.Path(inputspecsfile).read_text()
+  template=template.replace("#!info!#", str(pathlib.Path(apiinfofile).read_text()).rstrip())
+  template=template.replace("#!params!#", str(pathlib.Path(apiparamsfile).read_text()).rstrip())
+  template=template.replace("#!requestBodies!#", str(pathlib.Path(apirequestBodiesfile).read_text()).rstrip())
+  template=template.replace("#!schemas!#", str(pathlib.Path(apischemafile).read_text()).rstrip())
   print("Creating " + outf + " using koiosapi.yaml as template...")
   for e in examples["params"]:
     template=template.replace(str("##" + str(e) + "_param##"), str(examples["params"][e][str(network)]))
@@ -18,9 +26,9 @@ def populate_spec(network, outf):
     f.write(template)
 
 def main():
-  populate_spec("m", "koiosapi-mainnet.yaml")
-  populate_spec("t", "koiosapi-testnet.yaml")
-  populate_spec("g", "koiosapi-guild.yaml")
+  populate_spec("m", "results/koiosapi-mainnet.yaml")
+  populate_spec("t", "results/koiosapi-testnet.yaml")
+  populate_spec("g", "results/koiosapi-guild.yaml")
   print("Done!!")
 
 try:
