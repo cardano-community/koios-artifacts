b4d52c1bbba8b71619bbfacfa65d4e568a293c81
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2022-05-09 12:47:06 +0100

    Add trusted instance determination + improvements

--- tests/data-test.sh
+++ tests/data-test.sh
@@ -1,31 +1,69 @@
 #!/usr/bin/env bash
 
 HAPROXY_SOCKET_PATH="/opt/cardano/cnode/sockets/haproxy.socket"
-VENV_DIR_PATH="/opt/cardano/cnode/tests/koios-artifacts/tests/koios-tests"
-API_SPEC_PATH="/opt/cardano/cnode/tests/koios-artifacts/specs/results/koiosapi-guild.yaml"
+TESTS_DIR_PATH="/opt/cardano/cnode/koios-artifacts/tests"
+VENV_DIR_PATH="${TESTS_DIR_PATH}/koios-tests"
+API_SPEC_PATH="/opt/cardano/cnode/koios-artifacts/specs/results/koiosapi-guild.yaml"
 
 source "${VENV_DIR_PATH}/bin/activate"
 
-# This is pseudo-code, need to from haproxy:
-# 1) ssl vs no ssl servers
-# 2) correct port combination for server
-# 3) correct server name (for toggling MAINT state)
-# Below line just gets resolved server IP addresses into array
-servers=$(echo "show servers state" | nc -U "$HAPROXY_SOCKET_PATH" | grep grest_core | cut -d' ' -f5)
-mapfile -t serversArray <<<"${servers}"
+# Determine a healthy instance to run checks against
+TRUSTED_INSTANCES=("http://127.0.0.1:8053" "https://koios-guild.ahlnet.nu:2165" "http://95.216.188.94:8053")
+for ((i = 0; i < ${#TRUSTED_INSTANCES[@]} - 1; i++)); do
+  echo "Comparing index: ${i}, instance: ${TRUSTED_INSTANCES[$i]}"
+  for compare_instance in "${TRUSTED_INSTANCES[@]:${i}+1}"; do
+    echo "  Comparing against: ${compare_instance}"
+    pytest ${TESTS_DIR_PATH} \
+      --local-url "${TRUSTED_INSTANCES[$i]}"/api/v0 \
+      --compare-url "${compare_instance}"/api/v0 \
+      --api-schema-file "${API_SPEC_PATH}" -x -q
+    result_code=$?
 
-for server in "${serversArray[@]}"; do
-  echo "Running data tests for: $server"
-  pytest /opt/cardano/cnode/tests/koios-artifacts/tests \
-    --local-url https://eden-guildnet.koios.rest:8453/api/v0/ \
-    --compare-url https://guild.koios.rest/api/v0/ \
-    --api-schema-file /opt/cardano/cnode/tests/koios-artifacts/specs/results/koiosapi-guild.yaml -x -v
-  resultCode=$?
+    if [ $result_code -eq 0 ]; then
+      HEALTHY_INSTANCE1=${TRUSTED_INSTANCES[$i]}
+      HEALTHY_INSTANCE2=${compare_instance}
+      break 2
+    fi
+  done
+done
 
-  echo "Result code: $resultCode"
-  if [[ "$resultCode" -ne 0 ]]; then
-    echo "Marking server for maintenance: $server"
-    # Need to get the correct ${server_name} here from haproxy
-    echo "disable server grest_core/${server_name}" | nc -U "$HAPROXY_SOCKET_PATH"
+if [ -z "${HEALTHY_INSTANCE1}" ]; then
+  echo "Could not deteremine a healthy instance to compare against!"
+  exit 1
+fi
+echo "Healthy instance determined: ${HEALTHY_INSTANCE1}"
+
+# Test each server and mark down/bring up from maintenance
+readarray -t servers_array < <(echo "show servers state grest_core" | nc -U "$HAPROXY_SOCKET_PATH" | grep 'grest_core' | awk '{print $4 " " $5 " " $18 " " $19 " " $7}')
+for server in "${servers_array[@]}"; do
+  IFS=" " read name ip dns port state <<<"${server}"
+  if [[ $name == *-ssl ]]; then
+    test_instance="https://"${dns}:${port}
+  else
+    test_instance="http://"${ip}:${port}
+  fi
+
+  if [[ "${test_instance}" == "${HEALTHY_INSTANCE1}" || "${test_instance}" == "${HEALTHY_INSTANCE2}" ]]; then
+    continue
+  fi
+
+  echo "Running data tests for: ${name} ${ip} ${dns} ${port}"
+
+  pytest ${TESTS_DIR_PATH} \
+    --local-url "${test_instance}"/api/v0 \
+    --compare-url "${HEALTHY_INSTANCE1}"/api/v0 \
+    --api-schema-file "${API_SPEC_PATH}" -x -q
+  result_code=$?
+
+  if [[ "$result_code" -ne 0 ]]; then
+    if [[ "${state}" -ne 1 ]]; then
+      echo "Marking server down for maintenance: ${name}"
+      echo "disable server grest_core/${name}" | nc -U "$HAPROXY_SOCKET_PATH"
+    else
+      echo "Server already marked down for maintenance: ${name}"
+    fi
+  elif [[ "${state}" -ne 0 ]]; then
+    echo "Marking server back up from maintenance: ${name}"
+    echo "enable server grest_core/${name}" | nc -U "$HAPROXY_SOCKET_PATH"
   fi
 done
--- tests/setup-tests.sh
+++ tests/setup-tests.sh
@@ -15,16 +15,16 @@ cat <<-EOF
 	  schemathesis --pre-run not_empty_response run --request-timeout 5000 --hypothesis-seed 1 https://guild.koios.rest/koiosapi.yaml \\
 	               --hypothesis-phases=explicit -v --hypothesis-verbosity quiet -b http://127.0.0.1:8053/api/v0 -c all
 	
-	      where http://127.0.0.1:8053/api/v0 is the URL of instance you want to test, and guild.koios.rest is the target enviornment for testing
+	      where http://127.0.0.1:8053/api/v0 is the URL of instance you want to test, and guild.koios.rest is the target enviornment for testing.
 	
 	To run the data validations tests, use the below:
 	
 	  pytest --local-url http://127.0.0.1:8053/api/v0 --compare-url https://guild.koios.rest/api/v0 --api-schema-file ../specs/results/koiosapi-guild.yaml -x -v
 	
 	  Arguments:
-	      local-url		:	URL of instance you want to test"
-	      compare-url	:	Source-of-truth instance to compare returned data against"
-	      api-schema-file	:	The API specs/schema file you want to use as input for validation"
+	      local-url		:	"URL of instance you want to test"
+	      compare-url	:	"Source-of-truth instance to compare returned data against"
+	      api-schema-file	:	"The API specs/schema file you want to use as input for validation"
 	
 	To enter Python virtualenv, type 'source koios-tests/bin/activate'
 	To exit from Python virtualenv, you can run 'deactivate' 
