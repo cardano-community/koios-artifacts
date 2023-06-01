3878798a1ab0f8145422aaac56e910346e958968
rdlrt <3169068+rdlrt@users.noreply.github.com>
2023-06-01 05:58:17 +0000

    Update [most] cron jobs to exit if the database has not received block in 5 mins

--- files/grest/cron/jobs/pool-history-cache-update.sh
+++ files/grest/cron/jobs/pool-history-cache-update.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 DB_NAME=cexplorer
 
+tip=$(psql ${DB_NAME} -qbt -c "select time from block order by id desc limit 1;")
+
+if [[ $(( $(date +%s) - $(TZ=UTC date --date="${tip}" +%s) )) -gt 300 ]]; then
+  echo "$(date +%F_%H:%M:%S) Skipping as database has not received a new block in past 300 seconds!" && exit 1
+fi
+
 echo "$(date +%F_%H:%M:%S) Running pool history cache update..."
-psql ${DB_NAME} -qbt -c "SELECT GREST.pool_history_cache_update();" 2>&1 1>/dev/null
+psql ${DB_NAME} -qbt -c "SELECT GREST.pool_history_cache_update();" 1>/dev/null 2>&1
 echo "$(date +%F_%H:%M:%S) Job done!"
