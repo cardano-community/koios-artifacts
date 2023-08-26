417fb1c4d07bc150d85f58ab0676da7f5ea77198
KoT_B_KocMoce <49576827+hodlonaut@users.noreply.github.com>
2023-08-26 20:39:43 +1000

    Shell script to invoke epoch summary corrections
    To be invoked via cron job every 10 minutes

---
+++ files/grest/cron/jobs/epoch-summary-corrections-update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+DB_NAME=cexplorer
+
+tip=$(psql ${DB_NAME} -qbt -c "select extract(epoch from time)::integer from block order by id desc limit 1;" | xargs)
+
+if [[ $(( $(date +%s) - tip )) -gt 300 ]]; then
+  echo "$(date +%F_%H:%M:%S) Skipping as database has not received a new block in past 300 seconds!" && exit 1
+fi
+
+echo "$(date +%F_%H:%M:%S) Running epoch summary corrections update..."
+psql ${DB_NAME} -qbt -c "SELECT GREST.EPOCH_SUMMARY_CORRECTIONS_UPDATE();" 1>/dev/null 2>&1
+echo "$(date +%F_%H:%M:%S) Job done!"
