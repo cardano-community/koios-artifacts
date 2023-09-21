0da242214ad09c1a3e7066723db0b6e5721a48eb
Priyank <3169068+rdlrt@users.noreply.github.com>
2023-09-21 14:17:37 +1000

    Add reserve/treasury withdrawals and pool_registrations/deregistrations

---
+++ files/grest/rpc/pool/pool_retirements.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE FUNCTION grest.pool_retirements(_epoch_no numeric)
+RETURNS TABLE (
+  pool_id_bech32 text,
+  tx_hash text,
+  block_hash text,
+  block_height word31type,
+  epoch_no word31type,
+  epoch_slot word31type,
+  active_epoch_no word31type
+)
+LANGUAGE SQL STABLE
+AS $$
+  SELECT
+    ph.view,
+    ENCODE(tx.hash,'hex'),
+    ENCODE(b.hash,'hex'),
+    b.block_no,
+    b.epoch_no,
+    b.epoch_slot_no,
+    pr.retiring_epoch
+  FROM pool_retire AS pr
+    LEFT JOIN tx ON pr.announced_tx_id = tx.id
+    INNER JOIN block AS b ON tx.block_id = b.id
+    LEFT JOIN pool_hash AS ph ON ph.id = pr.hash_id
+  WHERE b.epoch_no = _epoch_no;
+$$;
+
+COMMENT ON FUNCTION grest.pool_registrations IS 'A list of all pool retirements initiated in the requested epoch'; --noqa: LT01
