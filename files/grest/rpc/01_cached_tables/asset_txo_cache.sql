318adb05f22f545cd389f030cf25cfeb8f05a0e6
RdLrT <3169068+rdlrt@users.noreply.github.com>
2024-01-03 09:30:51 +1100

    Patch asset txo cache (#262)
    ## Description
    <!--- Describe your changes -->
    asset_tx_out_cache: We had a redundant condition for adding initial
    population of asset, but it was already being catered to since we had
    COALESCE condition checking existing entries of asset_tx_out_cache table
    
    ## Which issue it fixes?
    <!--- Link to issue: Closes #issue-number -->
    Closes #260 
    
    ## How has this been tested?
    <!--- Describe how you tested changes -->
    Instance providers have it applied on mainnet

--- files/grest/rpc/01_cached_tables/asset_txo_cache.sql
+++ files/grest/rpc/01_cached_tables/asset_txo_cache.sql
@@ -13,38 +13,17 @@ BEGIN
     RAISE EXCEPTION 'Previous asset_txo_cache_update query still running but should have completed! Exiting...';
   END IF;
 
-  CREATE TEMP TABLE tmp_ma AS (
-    SELECT ma1.id
-    FROM grest.asset_cache_control AS acc1
-      LEFT JOIN multi_asset AS ma1 ON ma1.policy = acc1.policy
-      LEFT JOIN grest.asset_tx_out_cache AS atoc1 ON ma1.id = atoc1.ma_id
-    WHERE atoc1.ma_id IS NULL
-  );
-
   WITH
     ma_filtered AS
       (
-        (SELECT
+        SELECT
           mto.tx_out_id,
           mto.quantity,
           mto.ident
         FROM grest.asset_cache_control AS acc
           LEFT JOIN multi_asset AS ma ON ma.policy = acc.policy
           LEFT JOIN ma_tx_out AS mto ON mto.ident = ma.id
-        WHERE ma.id IN
-          (SELECT id FROM tmp_ma)
-        )
-        UNION ALL
-        (
-          SELECT
-            mto.tx_out_id,
-            mto.quantity,
-            mto.ident
-          FROM grest.asset_cache_control AS acc
-            LEFT JOIN multi_asset AS ma ON ma.policy = acc.policy
-            LEFT JOIN ma_tx_out AS mto ON mto.ident = ma.id
-          WHERE mto.tx_out_id > (SELECT COALESCE(MAX(atoc.txo_id),0) FROM grest.asset_tx_out_cache AS atoc)
-        )
+        WHERE mto.tx_out_id > (SELECT COALESCE(MAX(atoc.txo_id),0) FROM grest.asset_tx_out_cache AS atoc)
       )
   INSERT INTO grest.asset_tx_out_cache
     SELECT
@@ -62,7 +41,6 @@ BEGIN
         LEFT JOIN tx_out AS txo ON atoc.txo_id = txo.id
         WHERE txo.consumed_by_tx_in_id IS NOT NULL
           OR txo.id IS NULL);
-  DROP TABLE tmp_ma;
 
 END;
 $$;
