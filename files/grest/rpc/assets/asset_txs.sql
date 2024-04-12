930a5c5457b666b523008538cabfa3897df2c4b7
rdlrt <3169068+rdlrt@users.noreply.github.com>
2024-04-12 18:13:25 +1000

    Include transaction where asset is burnt , as it is outside of input/output view, and add a distinct on transaction hash

--- files/grest/rpc/assets/asset_txs.sql
+++ files/grest/rpc/assets/asset_txs.sql
@@ -36,21 +36,22 @@ BEGIN
       tx_hashes.block_no,
       EXTRACT(EPOCH FROM tx_hashes.time)::integer
     FROM (
-      SELECT DISTINCT ON (tx.hash,txo.index::smallint)
+      SELECT DISTINCT ON (tx.hash)
         tx.hash,
         block.epoch_no,
         block.block_no,
         block.time
       FROM ma_tx_out AS mto
+      LEFT JOIN ma_tx_mint AS mtm ON mto.ident = mtm.ident
       INNER JOIN tx_out AS txo ON txo.id = mto.tx_out_id
-      INNER JOIN tx ON tx.id = txo.tx_id
+      INNER JOIN tx ON tx.id = txo.tx_id OR tx.id = mtm.tx_id
       INNER JOIN block ON block.id = tx.block_id
       WHERE
         mto.ident = _asset_id
         AND block.block_no >= _after_block_height
         AND (_history = TRUE OR txo.consumed_by_tx_id IS NULL)
       GROUP BY
-        ident,
+        mto.ident,
         tx.hash,
         txo.index::smallint,
         block.epoch_no,
