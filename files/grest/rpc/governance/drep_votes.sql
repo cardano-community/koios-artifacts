93fa77db26b3fd157d9d717b6170cb2166e1f040
Ola <ola.ahlman@gmail.com>
2024-07-27 00:26:12 +0200

    add drep_votes

---
+++ files/grest/rpc/governance/drep_votes.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE FUNCTION grest.drep_votes(_drep_id text)
+RETURNS TABLE (
+  tx_hash text,
+  cert_index integer,
+  block_time integer,
+  vote text
+)
+LANGUAGE sql STABLE
+AS $$
+  SELECT
+    ENCODE(tx.hash, 'hex')::text AS tx_hash,
+    vp.index AS cert_index,
+    EXTRACT(EPOCH FROM b.time)::integer AS block_time,
+    vp.vote
+  FROM public.drep_hash AS dh
+    INNER JOIN public.voting_procedure AS vp ON dh.id = vp.drep_voter
+    INNER JOIN public.tx ON vp.tx_id = tx.id
+    INNER JOIN public.block b ON tx.block_id = b.id
+  WHERE
+    dh.view = _drep_id
+  ORDER BY
+    block_time DESC;
+$$;
+
+COMMENT ON FUNCTION grest.drep_metadata IS 'Get all DRep votes cast from specified DRep ID'; -- noqa: LT01
