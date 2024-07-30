f33b5e9f4f71210d763eeccc1c646d070364a987
Ola <ola.ahlman@gmail.com>
2024-07-30 11:23:31 +0200

    fix drep_info active state and update specs

--- files/grest/rpc/governance/drep_info.sql
+++ files/grest/rpc/governance/drep_info.sql
@@ -6,31 +6,110 @@ RETURNS TABLE (
   registered boolean,
   deposit text,
   active boolean,
+  expires_epoch_no numeric,
   amount text
 )
 LANGUAGE plpgsql
 AS $$
 DECLARE
   curr_epoch    word31type;
+  drep_list     bigint[];
+  drep_activity word64type;
 BEGIN
 
   SELECT INTO curr_epoch MAX(epoch_no) FROM public.block;
 
-  RETURN QUERY
-  SELECT
-    DISTINCT ON (dh.view) dh.view AS drep_id,
-    ENCODE(dh.raw, 'hex')::text AS hex,
-    dh.has_script AS has_script,
-    (CASE WHEN (dr.deposit IS NOT NULL AND dr.deposit >= 0) OR starts_with(dh.view,'drep_') THEN TRUE ELSE FALSE END) AS registered,
-    dr.deposit::text AS deposit,
-    (CASE WHEN (dd.active_until IS NOT NULL AND dd.active_until > curr_epoch) OR starts_with(dh.view,'drep_') THEN TRUE ELSE FALSE END) AS active,
-    COALESCE(dd.amount, 0)::text AS amount
-  FROM public.drep_hash AS dh
-    LEFT JOIN public.drep_registration AS dr ON dh.id = dr.drep_hash_id
-    LEFT JOIN public.drep_distr AS dd ON dh.id = dd.hash_id AND dd.epoch_no = curr_epoch
-  WHERE dh.view = ANY(_drep_ids)
-  ORDER BY
-    dh.view, dr.tx_id DESC;
+  SELECT INTO drep_activity ep.drep_activity FROM public.epoch_param AS ep WHERE ep.epoch_no = curr_epoch;
+
+  -- all DRep ids
+  SELECT INTO drep_list ARRAY_AGG(id)
+  FROM (
+    SELECT id
+    FROM public.drep_hash
+    WHERE view = ANY(_drep_ids)
+  ) AS tmp;
+
+  RETURN QUERY (
+    WITH
+
+      _last_registration AS (
+        SELECT
+          DISTINCT ON (dr.drep_hash_id) drep_hash_id AS drep,
+          dr.tx_id,
+          dr.deposit
+        FROM
+          public.drep_registration AS dr
+        WHERE
+          dr.drep_hash_id = ANY(drep_list)
+          AND (dr.deposit IS NOT NULL AND dr.deposit >= 0)
+        ORDER BY
+          dr.drep_hash_id, dr.tx_id DESC
+      ),
+
+      _last_update AS (
+        SELECT
+          dr.drep_hash_id AS drep,
+          MAX(dr.tx_id) AS tx_id
+        FROM
+          public.drep_registration AS dr
+        WHERE
+          dr.drep_hash_id = ANY(drep_list)
+          AND dr.deposit IS NULL
+        GROUP BY dr.drep_hash_id
+      ),
+
+      _last_vote AS (
+        SELECT
+          drep_voter AS drep,
+          MAX(tx_id) AS tx_id
+        FROM
+          public.voting_procedure
+        WHERE
+          drep_voter = ANY(drep_list)
+        GROUP BY drep_voter
+      ),
+
+      _drep_state AS (
+        SELECT
+          drep_state.drep,
+          drep_state.deposit,
+          (CASE WHEN (curr_epoch - b.epoch_no) < drep_activity THEN TRUE ELSE FALSE END) AS active,
+          (b.epoch_no + drep_activity) AS expires_epoch_no
+        FROM (
+          SELECT
+            all_activities.drep,
+            MAX(all_activities.tx_id) AS last_tx_id,
+            MAX(all_activities.deposit) AS deposit
+          FROM (
+            SELECT lr.drep, lr.tx_id, lr.deposit FROM _last_registration AS lr
+            UNION ALL
+            SELECT lu.drep, lu.tx_id, 0 AS deposit FROM _last_update AS lu
+            UNION ALL
+            SELECT lv.drep, lv.tx_id, 0 AS deposit FROM _last_vote AS lv
+          ) AS all_activities
+          GROUP BY all_activities.drep
+        ) AS drep_state
+        INNER JOIN tx on drep_state.last_tx_id = tx.id
+        INNER JOIN block AS b ON tx.block_id = b.id
+      )
+
+    SELECT
+      DISTINCT ON (dh.view) dh.view AS drep_id,
+      ENCODE(dh.raw, 'hex')::text AS hex,
+      dh.has_script AS has_script,
+      (CASE WHEN starts_with(dh.view,'drep_') OR (COALESCE(dr.deposit, 0) >= 0 AND dr.drep_hash_id IS NOT NULL) THEN TRUE ELSE FALSE END) AS registered,
+      (CASE WHEN (dr.deposit < 0) OR starts_with(dh.view,'drep_') THEN 0 ELSE COALESCE(ds.deposit, 0) END)::text AS deposit,
+      (CASE WHEN starts_with(dh.view,'drep_') THEN TRUE ELSE COALESCE(dr.deposit, 0) >= 0 AND ds.active END) AS active,
+      (CASE WHEN COALESCE(dr.deposit, 0) >= 0 THEN COALESCE(ds.expires_epoch_no, 0) ELSE 0 END) AS expires_epoch_no,
+      COALESCE(dd.amount, 0)::text AS amount
+    FROM public.drep_hash AS dh
+      LEFT JOIN public.drep_registration AS dr ON dh.id = dr.drep_hash_id
+      LEFT JOIN public.drep_distr AS dd ON dh.id = dd.hash_id AND dd.epoch_no = curr_epoch
+      LEFT JOIN _drep_state AS ds ON dh.id = ds.drep
+    WHERE dh.id = ANY(drep_list)
+    ORDER BY
+      dh.view, dr.tx_id DESC
+  );
 
 END;
 $$;
