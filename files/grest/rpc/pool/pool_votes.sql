7b13500de686842e78db07aa0e85d648ab89a129
Greg B <adahoskinson@gmail.com>
2024-08-02 15:31:01 +0800

    Synced up pool_votes endpoint with the committee/drep votes, logic/field wise

--- files/grest/rpc/pool/pool_votes.sql
+++ files/grest/rpc/pool/pool_votes.sql
@@ -1,25 +1,28 @@
 CREATE OR REPLACE FUNCTION grest.pool_votes(_pool_bech32 text)
 RETURNS TABLE (
-  tx_hash text,
+  proposal_tx_hash text,
   cert_index integer,
+  vote_tx_hash text,
   block_time integer,
   vote text
 )
 LANGUAGE sql STABLE
 AS $$
   SELECT
-    ENCODE(tx.hash, 'hex')::text AS tx_hash,
+    ENCODE(prop_tx.hash, 'hex')::text AS proposal_tx_hash,
     gap.index AS cert_index,
+    ENCODE(vote_tx.hash, 'hex')::text AS vote_tx_hash,
     EXTRACT(EPOCH FROM b.time)::integer AS block_time,
     vp.vote
   FROM public.pool_hash ph
     INNER JOIN public.voting_procedure AS vp ON ph.id = vp.pool_voter
     INNER JOIN public.gov_action_proposal AS gap ON vp.gov_action_proposal_id = gap.id
-    INNER JOIN public.tx ON gap.tx_id = tx.id
-    INNER JOIN public.block AS b ON tx.block_id = b.id
+    INNER JOIN public.tx prop_tx ON gap.tx_id = prop_tx.id
+    INNER JOIN public.tx vote_tx on vp.tx_id = vote_tx.id
+    INNER JOIN public.block AS b ON vote_tx.block_id = b.id
   WHERE ph.view = _pool_bech32
   ORDER BY
-    block_time DESC;
+    vote_tx.id DESC;
 $$;
 
 COMMENT ON FUNCTION grest.pool_votes IS 'Get all SPO votes cast for a given pool'; -- noqa: LT01
