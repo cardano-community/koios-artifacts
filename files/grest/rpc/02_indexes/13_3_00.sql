0ee85fac624959f2350c09b6bf1709774b016acb
rdlrt <3169068+rdlrt@users.noreply.github.com>
2024-08-25 23:54:25 +1000

    Minor fix to specs and index

--- files/grest/rpc/02_indexes/13_3_00.sql
+++ files/grest/rpc/02_indexes/13_3_00.sql
@@ -1,5 +1,6 @@
 CREATE INDEX IF NOT EXISTS pool_stat_pool_hash_id ON pool_stat(pool_hash_id);
 CREATE INDEX IF NOT EXISTS pool_stat_epoch_no ON pool_stat(epoch_no);
-CREATE INDEX IF NOT EXISTS idx_drep_hash_view ON drep_hash (view);
+-- CREATE INDEX IF NOT EXISTS idx_drep_hash_view ON drep_hash (view);
+CREATE INDEX IF NOT EXISTS idx_drep_hash_raw ON drep_hash (raw);
 CREATE INDEX IF NOT EXISTS idx_reward_rest_addr_id ON reward_rest (addr_id);
 CREATE INDEX IF NOT EXISTS idx_reward_rest_spendable_epoch ON reward_rest (spendable_epoch);
