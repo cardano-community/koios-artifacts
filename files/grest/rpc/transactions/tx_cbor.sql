b5473d5b5dd04baece80e923bcf1848a3ac53d6f
hodlonaut <hodlonaut@github.com>
2025-01-03 12:09:02 +1100

    Updated the schema for the response of proposal voting summary endpoint

--- files/grest/rpc/transactions/tx_cbor.sql
+++ files/grest/rpc/transactions/tx_cbor.sql
@@ -31,3 +31,34 @@ AS $$
 $$;
 
 COMMENT ON FUNCTION grest.tx_cbor IS 'Get raw transaction(s) in CBOR format'; -- noqa: LT01
+
+CREATE OR REPLACE FUNCTION grest.block_tx_cbor(_block_hashes text [])
+RETURNS TABLE (
+  tx_hash text,
+  block_hash text,
+  block_height word31type,
+  epoch_no word31type,
+  absolute_slot word63type,
+  tx_timestamp integer,
+  cbor text
+)
+LANGUAGE sql STABLE
+AS $$
+  SELECT *
+  FROM grest.tx_cbor(
+      (
+        SELECT ARRAY_AGG(ENCODE(hash, 'hex'))
+        FROM  tx
+        WHERE tx.block_id = ANY(
+          SELECT id
+          FROM block
+          WHERE hash = ANY(
+            SELECT DISTINCT DECODE(hashes_hex, 'hex')
+            FROM UNNEST(_block_hashes) AS hashes_hex
+          )
+        )
+      )
+    );
+$$;
+
+COMMENT ON FUNCTION grest.block_tx_cbor IS 'Get Raw transaction in CBOR format for given block hashes.'; -- noqa: LT01
