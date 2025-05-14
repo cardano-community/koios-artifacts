bf417e94e74f9d7b7ec5b0ad28f170677cf461d3
hodlonaut <hodlonaut@github.com>
2025-05-14 17:51:34 +1000

    Following feedback from Priyank, change upper bound of between from current epoch to last populated epoch in epoch_stake

--- files/grest/rpc/pool/pool_owner_history.sql
+++ files/grest/rpc/pool/pool_owner_history.sql
@@ -20,12 +20,17 @@ AS $$
       AND NOT EXISTS (SELECT 1 FROM pool_update pu2 WHERE pu2.hash_id = pu.hash_id AND pu2.active_epoch_no = pu.active_epoch_no AND pu2.id > pu.id)
     INNER JOIN pool_owner AS po ON po.pool_update_id = pu.id
     INNER JOIN stake_address AS sa ON po.addr_id = sa.id
-    INNER JOIN epoch_stake AS es ON po.addr_id = es.addr_id AND es.pool_id = ph.id
-    -- only interested in active stake of owners between epoch in which they were activated
-    -- via pool_update and the epoch before the next pool_update came into effect,
-    -- or current epoch if there haven't been further updates
+    INNER JOIN epoch_stake AS es ON po.addr_id = es.addr_id
+      AND es.pool_id = ph.id
+      -- only interested in active stake of owners between epoch in which they were activated
+      -- via pool_update and the epoch before the next pool_update came into effect,
+      -- or last populated epoch in epoch_stake if there haven't been further updates
       AND es.epoch_no BETWEEN pu.active_epoch_no AND
-      (SELECT COALESCE(MIN(pu3.active_epoch_no) - 1, (SELECT MAX(no) FROM epoch)) FROM pool_update pu3 WHERE pu3.hash_id = pu.hash_id AND pu3.id > pu.id)
+        (SELECT COALESCE(
+            MIN(pu3.active_epoch_no) - 1,
+            (SELECT MAX(epoch_no) FROM epoch_stake_progress where completed)
+          )
+          FROM pool_update pu3 WHERE pu3.hash_id = pu.hash_id AND pu3.id > pu.id)
   WHERE ph.id IN (SELECT id FROM pool_hash AS ph WHERE ph.hash_raw = ANY(
         SELECT cardano.bech32_decode_data(p)
         FROM UNNEST(_pool_bech32_ids) AS p))
