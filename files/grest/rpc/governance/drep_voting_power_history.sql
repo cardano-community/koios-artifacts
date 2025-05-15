cbd831a4a3443c47cc07f888f956ce809cab8a06
KoT_B_KocMoce <49576827+hodlonaut@users.noreply.github.com>
2025-05-16 09:51:09 +1000

    Fix for #335 - addressing gaps in drep voting power history due to zero Ada totals (#336)
    ## Description
    Changed the INNER JOIN with drep_distr to LEFT OUTER JOIN and using
    epoch table's range from first registration epoch of a given drep to
    fill in the gaps with 0 balance
    
    ## Which issue it fixes?
    Closes #335
    
    ---------
    
    Co-authored-by: hodlonaut <hodlonaut@github.com>
    Co-authored-by: rdlrt <3169068+rdlrt@users.noreply.github.com>

--- files/grest/rpc/governance/drep_voting_power_history.sql
+++ files/grest/rpc/governance/drep_voting_power_history.sql
@@ -11,17 +11,33 @@ AS $$
       WHEN dh.raw IS NULL THEN dh.view
       ELSE grest.cip129_hex_to_drep_id(dh.raw, dh.has_script)
     END AS drep_id,
-    dd.epoch_no,
-    dd.amount::text AS amount
-  FROM public.drep_distr AS dd
-    INNER JOIN public.drep_hash AS dh ON dh.id = dd.hash_id
-  WHERE (CASE WHEN _epoch_no IS NULL THEN TRUE ELSE dd.epoch_no = _epoch_no END)
+    COALESCE(dd.epoch_no, e.no) as epoch_no,
+    COALESCE(dd.amount::text, '0') AS amount
+  FROM public.drep_hash AS dh
+  INNER JOIN public.epoch AS e on e.no > (
+    -- earliest voting power can be recorded for epoch after very first registration's epoch
+    SELECT b.epoch_no
+    FROM drep_registration AS dr
+    INNER JOIN tx AS t ON dr.drep_hash_id = dh.id AND dr.tx_id = t.id 
+    INNER JOIN block AS b ON t.block_id = b.id 
+    WHERE NOT EXISTS (
+      SELECT 1
+      FROM drep_registration AS dr2
+      WHERE dr2.drep_hash_id = dr.drep_hash_id AND dr2.id < dr.id
+    )
+  )
+  -- previously was doing INNER JOIN of drep_distr with drep_hash but if zero voting power drep_distr not created
+  LEFT OUTER JOIN public.drep_distr AS dd on dh.id = dd.hash_id AND e.no = dd.epoch_no
+  WHERE (CASE
+        WHEN _epoch_no IS NULL
+        THEN TRUE ELSE dd.epoch_no = _epoch_no
+      END)
     AND (CASE
           WHEN _drep_id IS NULL THEN TRUE
           WHEN STARTS_WITH(_drep_id,'drep_always') THEN dh.view = _drep_id
           ELSE dh.raw = DECODE(grest.cip129_drep_id_to_hex(_drep_id), 'hex')
         END)
-  ORDER BY dd.epoch_no DESC;
+  ORDER BY COALESCE(dd.epoch_no, e.no) DESC;
 $$;
 
 COMMENT ON FUNCTION grest.drep_voting_power_history IS 'Get history for dreps voting power distribution'; --noqa: LT01
