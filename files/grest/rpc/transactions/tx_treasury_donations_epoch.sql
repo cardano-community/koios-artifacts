757c740db49abb83419959c810c5c6e24174b83e
Dostrelith <12885604+dostrelith678@users.noreply.github.com>
2025-05-26 23:12:20 +0100

    fix: default NULL epoch input

--- files/grest/rpc/transactions/tx_treasury_donations_epoch.sql
+++ files/grest/rpc/transactions/tx_treasury_donations_epoch.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION grest.tx_treasury_donations_epoch(_epoch_no numeric)
+CREATE OR REPLACE FUNCTION grest.tx_treasury_donations_epoch(_epoch_no numeric DEFAULT NULL)
 RETURNS TABLE (
   epoch_no word31type,
   tx_hash text,
