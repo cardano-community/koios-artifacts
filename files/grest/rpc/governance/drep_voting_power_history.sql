5c2d125bb1decfc60c6e79130fc10ba83dddbb8c
gregbgithub <131832655+gregbgithub@users.noreply.github.com>
2025-05-27 14:17:39 +1000

    Update drep_voting_power_history.sql
    Added handling of pre-defined DReps when calculating the earliest epoch for which fetch the voting power

--- files/grest/rpc/governance/drep_voting_power_history.sql
+++ files/grest/rpc/governance/drep_voting_power_history.sql
@@ -15,16 +15,21 @@ AS $$
     COALESCE(dd.amount::text, '0') AS amount
   FROM public.drep_hash AS dh
   INNER JOIN public.epoch AS e on e.no > (
-    -- earliest voting power can be recorded for epoch after very first registration's epoch
-    SELECT b.epoch_no
-    FROM drep_registration AS dr
-    INNER JOIN tx AS t ON dr.drep_hash_id = dh.id AND dr.tx_id = t.id 
-    INNER JOIN block AS b ON t.block_id = b.id 
-    WHERE NOT EXISTS (
-      SELECT 1
-      FROM drep_registration AS dr2
-      WHERE dr2.drep_hash_id = dr.drep_hash_id AND dr2.id < dr.id
-    )
+    CASE 
+      WHEN dh.raw IS NULL THEN (SELECT MIN(epoch_no) - 1 FROM drep_distr AS predef_dd WHERE predef_dd.hash_id = dh.id)
+      ELSE (
+        -- earliest voting power can be recorded for epoch after very first registration's epoch
+        SELECT b.epoch_no
+        FROM drep_registration AS dr
+        INNER JOIN tx AS t ON dr.drep_hash_id = dh.id AND dr.tx_id = t.id 
+        INNER JOIN block AS b ON t.block_id = b.id 
+        WHERE NOT EXISTS (
+          SELECT 1
+          FROM drep_registration AS dr2
+          WHERE dr2.drep_hash_id = dr.drep_hash_id AND dr2.id < dr.id
+        )
+      ) 
+    END
   )
   -- previously was doing INNER JOIN of drep_distr with drep_hash but if zero voting power drep_distr not created
   LEFT OUTER JOIN public.drep_distr AS dd on dh.id = dd.hash_id AND e.no = dd.epoch_no
