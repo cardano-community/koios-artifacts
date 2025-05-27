1ffbfa1c4c3e4e2e7a6063599ed13273d2b48f94
Dostrelith <12885604+dostrelith678@users.noreply.github.com>
2025-05-27 01:52:39 +0100

    feat(rpc): Add treasury donations RPC (#346)
    ## Description
    <!--- Describe your changes -->
    Adds an endpoint to retrieve treasury donation transactions per epoch.
    
    ```
    cexplorer=# select * from grest.tx_treasury_donations_epoch(942);
    -[ RECORD 1 ]-----+-----------------------------------------------------------------
    epoch_no          | 942
    tx_hash           | 885393d0344ceffc748b7ada131d1ce44613044d8919fb6fb5f15f899a3cb1c9
    block_hash        | e86a70060a41ce1b4f5f247bbc98a0ed343d7e25fd7676217cfc434c113cb800
    block_index       | 1
    block_height      | 3263698
    block_time        | 1748048640
    treasury_donation | 1000000
    -[ RECORD 2 ]-----+-----------------------------------------------------------------
    epoch_no          | 942
    tx_hash           | 5b46990744aca4cb2fd9b7e047cc3ab8e3b86164bb2554cc47760721b641e01d
    block_hash        | c733929c4ffd54149b7fa359a0a9e4100a07a728237a05c3d4c026bd4998bd98
    block_index       | 1
    block_height      | 3263704
    block_time        | 1748048797
    treasury_donation | 2000000
    -[ RECORD 3 ]-----+-----------------------------------------------------------------
    epoch_no          | 942
    tx_hash           | 7413d3cdcb2255d5ac543bd3c4b9a32aa15cea2b9d55896ddde037be8dad8015
    block_hash        | a51d1a6d9deafe21219173d55287ec1d3c2d402f49af47203e7a176b029c859b
    block_index       | 0
    block_height      | 3263713
    block_time        | 1748048971
    treasury_donation | 3000000
    ```
    
    ## Which issue it fixes?
    <!--- Link to issue: Closes #issue-number -->
    Closes #345
    
    ---------
    
    Co-authored-by: RdLrT <3169068+rdlrt@users.noreply.github.com>

---
+++ files/grest/rpc/transactions/tx_treasury_donations_epoch.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE FUNCTION grest.tx_treasury_donations_epoch(_epoch_no numeric DEFAULT NULL)
+RETURNS TABLE (
+  epoch_no word31type,
+  tx_hash text,
+  block_hash text,
+  tx_block_index smallint,
+  block_height word31type,
+  block_time integer,
+  treasury_donation text
+)
+LANGUAGE sql STABLE
+AS $$
+  SELECT
+    b.epoch_no,
+    ENCODE(tx.hash, 'hex')::text AS tx_hash,
+    ENCODE(b.hash, 'hex')::text AS block_hash,
+    tx.block_index::smallint AS tx_block_index,
+    b.block_no AS block_height,
+    EXTRACT(EPOCH FROM b.time)::integer AS block_time,
+    tx.treasury_donation::text
+  FROM public.tx AS tx
+    INNER JOIN public.block AS b ON b.id = tx.block_id
+  WHERE b.epoch_no = COALESCE(_epoch_no, (SELECT MAX(no) FROM epoch))::word31type
+    AND tx.treasury_donation > 0
+  GROUP BY tx.id, tx.block_index, b.block_no, b.epoch_no, b.hash, b.time
+  ORDER BY tx.id ASC;
+$$;
+
+COMMENT ON FUNCTION grest.tx_treasury_donations_epoch IS 'Get a list of treasury donation transactions for a given epoch'; -- noqa: LT01
