6a42b2f9e177d84aaac199cf6738e67f54fb8c46
rdlrt <3169068+rdlrt@users.noreply.github.com>
2025-08-01 21:07:32 +1000

    Add era info to endpoints

--- files/grest/rpc/00_blockchain/tip.sql
+++ files/grest/rpc/00_blockchain/tip.sql
@@ -1,6 +1,7 @@
 CREATE OR REPLACE FUNCTION grest.tip()
 RETURNS TABLE (
   hash text,
+  era varchar,
   epoch_no word31type,
   abs_slot word63type,
   epoch_slot word31type,
@@ -13,12 +14,15 @@ AS $$
   SELECT
     ENCODE(b.hash::bytea, 'hex') AS block_hash,
     b.epoch_no AS epoch_no,
+    em.era,
     b.slot_no AS abs_slot,
     b.epoch_slot_no AS epoch_slot,
     b.block_no AS block_height,
     b.block_no AS block_no,
     EXTRACT(EPOCH FROM b.time)::integer
-  FROM block AS b
+  FROM public.block AS b
+  LEFT JOIN public.epoch_param AS ep ON ep.epoch_no = b.epoch_no
+  LEFT JOIN grest.era_map em ON ep.protocol_major::text = em.protocol_major::text AND ep.protocol_minor::text = em.protocol_minor::text
   ORDER BY b.id DESC
   LIMIT 1;
 $$;
